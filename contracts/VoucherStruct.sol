b9ca02e2be1438bdcff52bd5d31e2f3025d7bd75
VitalR <vitalii.rodikov@gmail.com>
2022-02-11 22:56:35 +0200

    Add voucher struct

---
+++ contracts/VoucherStruct.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+struct NFTVoucher {
+    /// @notice The id of the token to be redeemed. Must be unique - if another token with this ID already exists, the redeem function will revert.
+    uint256 tokenId;
+
+    /// @notice The minimum price (in wei) that the NFT creator is willing to accept for the initial sale of this NFT.
+    uint256 sellingPrice;
+
+    /// @notice Percentage basis-points for royalty on secondary sales, eg 495 == 4.95%
+    uint16 royaltyBasisPoints;
+
+    /// @notice The metadata URI to associate with this token.
+    string tokenUri;
+
+    /// @notice the EIP-712 signature of all other fields in the NFTVoucher struct. For a voucher to be valid, it must be signed by an account with the MINTER_ROLE.
+    bytes signature;
+}
