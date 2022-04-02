61b052befaf581ba1d66f731c78483adbba560d5
VitalR <vitalii.rodikov@gmail.com>
2022-04-02 23:02:57 +0300

    Update to support royalties

--- contracts/market/MarketNFT721ImplementationV2.sol
+++ contracts/market/MarketNFT721ImplementationV2.sol
@@ -8,8 +8,8 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./ERC2981/IERC2981Royalties.sol";
-import "./ERC2981/ERC2981Base.sol";
+import "../ERC2981/IERC2981Royalties.sol";
+import "../ERC2981/ERC2981.sol";
 import "./Ownable.sol";
 
 
